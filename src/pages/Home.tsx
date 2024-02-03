73e6a1f1d13e610299790fd1ef93fab966859dff
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-03 19:40:14 -0300

    feat: test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
         </h1>
 
         <p className="text-branco text-2xl max-w-sm mx-auto font-Montserrat bg-AzulEscuro bg-opacity-60 rounded  animate-fade-left animate-once animate-duration-[2000ms] animate-delay-[50ms]">
-          Venho me dedicando todos os dias em programação , com o objetivo de me tornar um desenvolvedor Full Stack excepcional. Meu foco é resolver problemas do mercado, e dar o meu melhor a cada dia independente dos problemas.
+          Venho me dedicando todos os dias em programação , com o objetivo de me tornar um desenvolvedor Full Stack excepcional. Meu foco é dar o meu melhor a cada dia independente dos problemas.
         </p>
 
         <a href="" className="bg-gradient-to-r from-ad to-azul-celeste shadow-sm shadow-ab hover:shadow-AzulEscuro text-white font-bold py-2 px-4 rounded-full w-36 hover:text-az flex flex-row justify-around items-center animate-fade-right animate-once animate-duration-[2000ms] animate-delay-[50ms] mt-5">
