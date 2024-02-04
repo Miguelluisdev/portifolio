f006847391f7e1493a217af01f2c38e47c5c4cf9
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-04 19:04:18 -0300

    feat: att

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,10 +4,10 @@ import TechImages from "./TechImages"
 const Skills = () => {
   return (
     <div className="container mx-auto px-4 pt-64" >
-      <h1 className="text-3xl font-Montserrat font-bold text-center ">
+      <h1 className="text-3xl font-Montserrat font-bold text-center animate-fade-left animate-once animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-forwards ">
         Minhas Habilidades
       </h1>
-      <span className="text-xl font-Montserrat text-center" >As tecnologias que estudo e trabalho</span>
+      <span className="text-xl font-Montserrat text-center animate-fade-left animate-once animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-forwards " >As tecnologias que estudo e trabalho</span>
       <TechImages images={ImagesArray} />
     </div>
   )
