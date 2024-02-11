ba2883e38b5c0a3f0801227da55618fe75058e41
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-11 16:47:26 -0300

    :recycle: refactor: new paragraph

--- src/components/Skills/TechImages.tsx
+++ src/components/Skills/TechImages.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+
 interface TechImage {
   techImage: string;
+  name: string; // Adicionando propriedade para o nome da linguagem
 }
 
 const TechImages: React.FC<{ images: TechImage[] }> = ({ images }) => {
   return (
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center items-center">
       {images.map((image) => (
-        <img
-          key={image.techImage}
-          src={image.techImage}
-          alt={image.techImage.replace('.svg', '')}
-          className="w-20 h-20 m-4 object-cover rounded-full animate-jump-in animate-once animate-duration-[2000ms] animate-ease-linear"
-        />
+        <div key={image.techImage} className="m-4 text-center">
+          <img
+            src={image.techImage}
+            alt={image.name}
+            className="w-20 h-20 object-cover rounded-full animate-jump-in animate-once animate-duration-[2000ms] animate-ease-linear"
+          />
+          <p className="text-xl mt-2 font-Montserrat">{image.name}</p>
+        </div>
       ))}
     </div>
   );
--- src/modules/ArrayImg.ts
+++ src/modules/ArrayImg.ts
@@ -1,39 +1,51 @@
-type TechImage  = {
-  techImage: string
-}
-export const ImagesArray: TechImage[] = [ 
+type TechImage = {
+  techImage: string;
+  name: string; 
+};
+
+export const ImagesArray: TechImage[] = [
   {
     techImage: '/html.svg',
-   
-  }, 
+    name: 'HTML', 
+  },
   {
     techImage: '/css.svg',
-  }, 
+    name: 'CSS', 
+  },
   {
     techImage: '/javascript.svg',
-  }, 
+    name: 'JavaScript', 
+  },
   {
     techImage: '/typescript.svg',
-  }, 
+    name: 'TypeScript', 
+  },
   {
     techImage: '/react.svg',
-  }, 
+    name: 'React', 
+  },
   {
     techImage: '/vite.svg',
-  }, 
+    name: 'Vite', 
+  },
   {
     techImage: '/git.svg',
-  }, 
+    name: 'Git', 
+  },
   {
     techImage: '/sass.svg',
-  }, 
+    name: 'Sass', 
+  },
   {
     techImage: '/bootstrap.svg',
-  }, 
+    name: 'Bootstrap', 
+  },
   {
     techImage: '/tailwindcss.svg',
-  }, 
+    name: 'Tailwind CSS', 
+  },
   {
     techImage: '/styled-components.svg',
-  }, 
-]
+    name: 'Styled Components', 
+  },
+];
