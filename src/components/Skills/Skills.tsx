73b7b9bb0a67b9c6cc8b29f32180c1fa66cea0f5
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-02-11 16:56:10 -0300

    style: padding altered

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,7 +5,7 @@ import bg from "../../assets/cyb.jpg"
 const Skills = () => {
   return (
     <div className="h-screen" style={{ backgroundImage: `url(${bg})`, backgroundBlendMode: 'darken', backgroundColor: 'rgba(0, 0, 0, 0.405)' }}>
-      <div className="container mx-auto px-4 pt-64">
+      <div className="container mx-auto px-4 pt-60">
         <h1 className="text-3xl font-Montserrat font-bold text-center animate-fade-left animate-once animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-forwards">
           Minhas Habilidades
         </h1>
